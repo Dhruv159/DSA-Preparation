421fa5de004e423a524ee26402a6425cf616834e
Dhruv Sharma <dhruvsarkhandia9@gmail.com>
2026-07-25 22:48:17 +0530

    25-07-26

---
+++ 25-07-26/Binary-search.ts
@@ -0,0 +1,20 @@
+function binarySearch(nums: any, target: any) {
+    let left = 0;
+    let right = nums.length -1;
+
+    while(left <= right) {
+        let mid = Math.floor((left + right)/2);
+
+        if(nums[mid] == target) {
+            return mid;
+        } else if(nums[mid] > target) {
+            right = mid - 1;
+        } else if(nums[mid] < target){
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+};
+
+binarySearch([2,3,4], 2)
