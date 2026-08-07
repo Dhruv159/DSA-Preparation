befa0ed183b1debf6133629e4c371076aecfc21f
Dhruv Sharma <dhruvsarkhandia9@gmail.com>
2026-08-07 23:42:37 +0530

    06-08-26

--- 26-07-26/Sort.ts
+++ 26-07-26/Sort.ts
@@ -1,11 +1,11 @@
-// function mergeSort(arr: any, ): void {
-//     let mid = Math.floor(arr.length/2);
+function mergeSort(arr: any, ): void {
+    let mid = Math.floor(arr.length/2);
 
-//     // let left = mergeSort(0, mid);
-//     // let right = mergeSort(mid)
-
-
+    // let left = mergeSort(0, mid);
+    // let right = mergeSort(mid)
 
 
 
-// }
+
+
+}
